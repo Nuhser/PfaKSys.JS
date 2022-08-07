553f1277108bdb508ede4a58df33fe30c9ae061b
Nuhser <martinimmel@gmail.com>
2022-08-07 09:30:24 +0200

    Add service for item conditions

---
+++ src/feature/Items/services/ItemCondition/index.js
@@ -0,0 +1,10 @@
+export const ItemCondition = {
+    UNKNOWN: 'items.conditions.unknown',
+    GOOD: 'items.conditions.good',
+    OK: 'items.conditions.ok',
+    MOSTLY_OK: 'items.conditions.mostlyOk',
+    DAMAGED: 'items.conditions.damaged',
+    IN_REPAIR: 'items.conditions.inRepair',
+    UNHYGIENIC: 'items.conditions.unhygienic',
+    OTHER: 'items.conditions.other'
+}
