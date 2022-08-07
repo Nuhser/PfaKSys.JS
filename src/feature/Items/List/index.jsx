6dc5ef60c3b6bd462752fceace14f64517201b89
Nuhser <martinimmel@gmail.com>
2022-08-07 09:31:05 +0200

    Add placeholder components for item and user lists

---
+++ src/feature/Items/List/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ItemList extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>This is the ItemList component!</p>
+            </div>
+        );
+    }
+}
---
+++ src/feature/Users/List/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class UserList extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>This is the UserList component!</p>
+            </div>
+        );
+    }
+}
