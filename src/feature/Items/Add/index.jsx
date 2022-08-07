b1c69d66a033827befef00aec3d0956d2c062e15
Nuhser <martinimmel@gmail.com>
2022-08-07 17:41:12 +0200

    Add inline help

--- src/feature/Items/Add/index.jsx
+++ src/feature/Items/Add/index.jsx
@@ -1,13 +1,18 @@
-import React from "react";
 import axios from "axios";
+import React from "react";
 import Button from "react-bootstrap/Button";
+import Col from 'react-bootstrap/Col';
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
+import Row from 'react-bootstrap/Row';
 import { withTranslation } from "react-i18next";
+import { BsQuestionCircle } from "react-icons/bs";
 import { trackPromise } from "react-promise-tracker";
 
-import { ItemCondition } from "../services/ItemCondition";
 import { setActiveSidebarItem } from '../../../components/Sidebar/service';
+import { ItemCondition } from "../services/ItemCondition";
 
 class AddItemForm extends React.Component {
     constructor(props) {
@@ -104,65 +109,105 @@ class AddItemForm extends React.Component {
 
                 <form onSubmit={this.onSubmit}>
                     <Form.Group className="mb-3" controlId="formName">
-                        <Form.Label>{t('common.name')}</Form.Label>
+                        <Form.Label>{t("common.name")}</Form.Label>
                         <Form.Control
                             type="text"
                             placeholder="Enter name"
                             value={this.state.name}
                             maxLength={100}
-                            onChange={this.onChangeName} />
+                            onChange={this.onChangeName}
+                        />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formQuantity">
-                        <Form.Label>{t('items.quantity')}</Form.Label>
-                        <InputGroup>
-                            <InputGroup.Checkbox
-                                checked={this.state.no_quantity}
-                                onChange={this.onChangeNoQuantity}
-                                aria-label="Item's quantity is unknown" />
-                            <Form.Control
-                                type="number"
-                                value={this.state.quantity}
-                                min={-1}
-                                onChange={this.onChangeQuantity}
-                                disabled={this.state.no_quantity} />
-                        </InputGroup>
+                        <Form.Label>{t("items.quantity")}</Form.Label>
+                            <Row>
+                                <Col>
+                                    <InputGroup>
+                                        <Form.Control
+                                            type="number"
+                                            value={this.state.quantity}
+                                            min={-1}
+                                            onChange={this.onChangeQuantity}
+                                            disabled={this.state.no_quantity}
+                                        />
+                                        <InputGroup.Text>{t('items.noQuantity')}</InputGroup.Text>
+                                        <InputGroup.Checkbox
+                                            checked={this.state.no_quantity}
+                                            onChange={this.onChangeNoQuantity}
+                                            aria-label="Item's quantity is unknown"
+                                        />
+                                    </InputGroup>
+                                </Col>
+
+                                <Col xs="auto">
+                                    <OverlayTrigger
+                                        className="ms-2"
+                                        trigger="hover"
+                                        placement="left"
+                                        overlay={
+                                            <Popover>
+                                                <Popover.Header as="h3">
+                                                    {t('items.noQuantityPopoverTitle')}
+                                                </Popover.Header>
+                                                <Popover.Body>
+                                                    {t('items.noQuantityPopoverBody')}
+                                                </Popover.Body>
+                                            </Popover>
+                                        }
+                                    >
+                                        <Button className="inline-help" variant="light">
+                                            <BsQuestionCircle size="20" />
+                                        </Button>
+                                    </OverlayTrigger>
+                                </Col>
+                            </Row>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formCondition">
-                        <Form.Label>{t('items.condition')}</Form.Label>
+                        <Form.Label>{t("items.condition")}</Form.Label>
                         <Form.Select
                             value={this.state.condition}
-                            onChange={this.onChangeCondition}>
-
-                            {this.state.conditions.map(
-                                (condition) => {
-                                    return (
-                                        <option key={condition} value={condition}>
-                                            {t(ItemCondition[condition])}
-                                        </option>
-                                    )
-                                }
-                            )}
+                            onChange={this.onChangeCondition}
+                        >
+                            {this.state.conditions.map((condition) => {
+                                return (
+                                    <option key={condition} value={condition}>
+                                        {t(ItemCondition[condition])}
+                                    </option>
+                                );
+                            })}
                         </Form.Select>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formDescription">
-                        <Form.Label>{t('common.description')}</Form.Label>
-                        <Form.Control 
+                        <Form.Label>{t("common.description")}</Form.Label>
+                        <Form.Control
                             as="textarea"
                             value={this.state.description}
                             rows={5}
                             maxLength={1000}
-                            onChange={this.onChangeDescription} />
+                            onChange={this.onChangeDescription}
+                        />
                     </Form.Group>
 
-                    <Button name="saveButton" variant="success" type="submit" onClick={this.onSubmit}>
-                        {t('common.save')}
+                    <Button
+                        name="saveButton"
+                        variant="success"
+                        type="submit"
+                        onClick={this.onSubmit}
+                    >
+                        {t("common.save")}
                     </Button>
 
-                    <Button className="ms-2" name="saveAndDetailsButton" variant="success" type="submit" onClick={this.onSubmit}>
-                        {t('common.saveAndDetails')}
+                    <Button
+                        className="ms-2"
+                        name="saveAndDetailsButton"
+                        variant="success"
+                        type="submit"
+                        onClick={this.onSubmit}
+                    >
+                        {t("common.saveAndDetails")}
                     </Button>
                 </form>
             </div>
