e902022af3fe458331ad34a507194ca431f3dce1
Nuhser <martinimmel@gmail.com>
2022-08-07 09:27:54 +0200

    Add i18n initialization

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { initReactI18next } from "react-i18next";
+
+import App from './components/App';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+i18n.use(initReactI18next)
+    .use(LanguageDetector)
+    .use(HttpApi)
+    .init({
+        fallbackLng: "en"
+    });
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
