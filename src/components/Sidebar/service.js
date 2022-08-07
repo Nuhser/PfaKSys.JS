c0da24ee078d32908970519e513298c69167d808
Nuhser <martinimmel@gmail.com>
2022-08-07 10:35:52 +0200

    Fix dropdown child highlighting

--- src/components/Sidebar/service.js
+++ src/components/Sidebar/service.js
@@ -1,12 +1,21 @@
 export function setActiveSidebarItem(id) {
-    var items = document.getElementsByClassName('sidebar-item');
-    var i;
-    
-    for (i = 0; i < items.length; i++) {
+    var dropdownContainer = document.getElementsByClassName('dropdown-container');
+    for (var i = 0; i < dropdownContainer.length; i++) {
+        dropdownContainer[i].style.display = 'none';
+    }
+
+
+    var items = document.getElementsByClassName('sidebar-item');    
+    for (var i = 0; i < items.length; i++) {
         items[i].classList.remove('active');
 
         if (items[i].id === id) {
             items[i].classList.add('active');
+
+            var parent = items[i].parentElement;
+            if (parent && parent.classList.contains('dropdown-container')) {
+                parent.style.display = 'block';
+            }
         }
     }
 }
