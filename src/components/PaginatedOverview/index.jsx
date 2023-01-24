2f3175d2d26100153937e10019b2269734243b5c
Nuhser <martinimmel@gmail.com>
2023-01-24 07:37:59 +0100

    Add component for paginated overview pages

---
+++ src/components/PaginatedOverview/index.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
+import { withRouter } from "../../services/CustomWrappers";
+
+class PaginatedOverview extends React.Component {
+    render() {
+        const { url, elements, total, page, limit, OverviewCardComponent, navigate } = this.props;
+        const pageCount = Math.ceil(total / limit);
+
+        return (
+            <div>
+                {
+                    elements.map(
+                        (element) => (
+                            <OverviewCardComponent
+                                key={element.id}
+                                element={element}
+                            />
+                        )
+                    )
+                }
+                
+                <div className="d-flex justify-content-center align-items-end mt-5">
+                    <ButtonGroup>
+                        <Button
+                            variant="secondary"
+                            onClick={() => {
+                                navigate(`${url}?page=${Math.max(0, parseInt(page)-1)}`);
+                            }}
+                            disabled={page == 0}
+                        >
+                            <FaChevronLeft size="18" />
+                        </Button>
+
+                        {
+                            [...Array(pageCount)].map(
+                                (e, i) => (
+                                    <Button
+                                        variant={page == i ? 'primary' : 'secondary'}
+                                        onClick={() => {
+                                            navigate(`${url}?page=${i}`);
+                                        }}
+                                        disabled={page == i}
+                                    >
+                                        {i+1}
+                                    </Button>
+                                )
+                            )
+                        }
+
+                        <Button
+                            variant="secondary"
+                            onClick={() => {
+                                navigate(`${url}?page=${Math.min(pageCount-1, parseInt(page)+1)}`);
+                            }}
+                            disabled={page == pageCount-1}
+                        >
+                            <FaChevronRight size="18" />
+                        </Button>
+                    </ButtonGroup>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default withRouter(PaginatedOverview);
