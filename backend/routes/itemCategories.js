f22b29b16097d62e2f7429d3e91d83f11fd7a3cc
Martin Immel <immel@weclapp.com>
2023-01-24 15:22:59 +0100

    Add missing API functions

--- backend/routes/itemCategories.js
+++ backend/routes/itemCategories.js
@@ -35,10 +35,10 @@ router.route('/').get(async (req, res) => {
                     .then(
                         count => res.json({itemCategories: categories, total: count, filter: categoryFilter})
                     ).catch(
-                        err => res.status(400).json('Error: ' + err)
+                        err => res.status(400).json(err)
                     );
             }
-        }).catch(err => res.status(400).json('Error: ' + err));
+        }).catch(err => res.status(400).json(err));
 });
 
 // POST new item category
@@ -51,15 +51,81 @@ router.route('/add').post((req, res) => {
 
     newItemCategory.save()
         .then(
-            (itemCategory) => res.json(
+            category => res.status(201).json(
                 {
-                    id: itemCategory._id,
+                    id: category._id,
                     message: `New item category '${name}' was created!`,
-                    itemCategory: itemCategory
+                    itemCategory: category
                 }
         )).catch(
-            err => res.status(400).json('Error: ' + err)
+            err => res.status(400).json(err)
         );
+});
+
+// GET total item category count
+router.route('/count').get(async (req, res) => {
+    let categoryFilter = createItemCategoryFilter(req);
+
+    await ItemCategory.count(categoryFilter)
+        .then(
+            count => res.json({total: count, filter: categoryFilter})
+        ).catch(
+            err => res.status(400).json(err)
+        );
+});
+
+// GET item category by id
+router.route('/:id').get((req, res) => {
+    ItemCategory.findById(req.params.id)
+        .then(
+            category => {
+                if (category === null) {
+                    res.status(404).json({name: 'IdNotFoundError', value: req.params.id, message: `No item category with the ID '${req.params.id}' found.`});
+                }
+                else {
+                    res.json(category);
+                }
+            }
+        ).catch(
+            err => res.status(400).json(err)
+        );
+});
+
+// UPDATE item category by id
+router.route('/:id').put((req, res) => {
+    ItemCategory.findByIdAndUpdate(req.params.id, req.body, {returnDocument: 'after', runValidators: true})
+        .then(
+            category => {
+                if (category === null) {
+                    res.status(404).json({name: 'IdNotFoundError', value: req.params.id, message: `No item category with the ID '${req.params.id}' found.`});
+                }
+                else {
+                    res.json(category);
+                }
+            }
+        ).catch(
+            err => res.status(400).json(err)
+        );
+});
+
+// DELETE itemcategory by id
+router.route('/:id').delete((req, res) => {
+    ItemCategory.findByIdAndRemove(req.params.id)
+        .then(category => {
+            if (category === null) {
+                res.status(404).json({name: 'IdNotFoundError', value: req.params.id, message: `No item category with the ID '${req.params.id}' found.`});
+            }
+            else {
+                res.json(
+                    {
+                        id: category._id,
+                        message: `Item category '${category.name}' was deleted!`,
+                        itemCategory: category
+                    }
+                );
+            }
+        })
+        .catch(err => res.status(400).json(err));
 });
 
 module.exports = router;
