23b6ab33b0a1a9e3b33016353de6946405b60e44
Nuhser <martinimmel@gmail.com>
2023-01-24 07:39:09 +0100

    Add item overview

--- src/feature/Items/List/index.jsx
+++ src/feature/Items/List/index.jsx
@@ -1,18 +1,63 @@
 import axios from "axios";
 import React from 'react';
+import { trackPromise } from "react-promise-tracker";
 
+import ItemOverviewCard from "../OverviewCard";
 import Main from "../../../components/Main";
+import PaginatedOverview from "../../../components/PaginatedOverview";
 import { setActiveSidebarItem } from '../../../components/Sidebar/service';
 import { withRouterAndTranslation } from "../../../services/CustomWrappers";
 
 class ItemList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+            total: 0,
+            page: 0,
+            limit: 10
+        };
+    }
+
+    getItemList() {
+        const { queryParams } = this.props;
+        const params = {
+            limit: queryParams.get('limit') || 10,
+            page: queryParams.get('page') || 0
+        };
+
+        this.setState({ page: params.page, limit: params.limit });
+
+        console.log('GET: /items', params);
+
+        trackPromise(axios.get(
+            'http://localhost:5000/items',
+            { params: params }
+        ).then(
+            res => {
+                this.setState({ items: res.data.items, total: res.data.total });
+            }
+        ).catch(
+            error => {
+                console.log(error);
+            }
+        ));
+    }
+
     componentDidMount() {
         setActiveSidebarItem('itemsOverviewSidebarItem');
+        this.getItemList();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.queryParams !== prevProps.queryParams) {
+            this.getItemList();
+        }
     }
 
     render() {
-        const { queryParams, t } = this.props;
-        const page = queryParams.get('page');
+        const { t } = this.props;
 
         return (
             <Main title={t('common.material')}>
@@ -20,7 +65,14 @@ class ItemList extends React.Component {
                 </Main.Header>
 
                 <Main.Body>
-                    <p>Hello World!</p>
+                    <PaginatedOverview
+                        url="/items"
+                        elements={this.state.items}
+                        total={this.state.total}
+                        page={this.state.page}
+                        limit={this.state.limit}
+                        OverviewCardComponent={ItemOverviewCard}
+                    />
                 </Main.Body>
             </Main>
         );
