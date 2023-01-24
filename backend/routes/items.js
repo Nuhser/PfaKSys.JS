08e4bf2d44b5aeffb2da4fe9728af0797a7b2eb9
Martin Immel <immel@weclapp.com>
2023-01-24 14:02:21 +0100

    Improve item filter for API calls

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -1,36 +1,66 @@
 const router = require('express').Router();
 let Item = require('../models/item.model');
 
+function createItemFilter(request) {
+    let itemFilter = {$and: []};
+
+    if (request.query.name) {
+        itemFilter['$and'].push({name: {$regex: request.query.name, $options: 'i'}});
+    }
+
+    if (request.query.inventory_id) {
+        itemFilter['$and'].push({inventory_id: {$regex: request.query.inventory_id, $options: 'i'}});
+    }
+    else {
+        itemFilter['$and'].push({$or: [{inventory_id: {$exists: false}}, {inventory_id: {$regex: '.*'}}]});
+    }
+
+    if (request.query.no_quantity) {
+        itemFilter['$and'].push({no_quantity: request.query.no_quantity});
+    }
+
+    if (request.query.quantity_min) {
+        itemFilter['$and'].push({quantity: {$gte: request.query.quantity_min}});
+    }
+
+    if (request.query.quantity_max) {
+        itemFilter['$and'].push({quantity: {$lte: request.query.quantity_max}});
+    }
+
+    if (request.query.categories) {
+        itemFilter['$and'].push({category: {$in: request.query.categories.split(',')}});
+    }
+
+    if (request.query.conditions) {
+        itemFilter['$and'].push({condition: {$in: request.query.conditions.split(',')}});
+    }
+
+    if (request.query.description) {
+        itemFilter['$and'].push({description: {$regex: request.query.description, $options: 'i'}});
+    }
+
+    return itemFilter;
+}
+
 // GET all items
 router.route('/').get(async (req, res) => {
-    // TODO: Change category-filter to an array and add filters for no_quantity, quantity_min and quantity_max
     let page = req.query.page;
     let limit = req.query.limit || 100;
-    let name = req.query.name || '.*';
-    let inventory_id = req.query.inventoy_id || '.*';
-    let category = req.query.category || '.*';
-    let conditions = req.query.condition;
-    let description = req.query.description || '.*';
+    let itemFilter = createItemFilter(req);
 
-    await Item.find({
-        name: { $regex: name, $options: 'i' },
-        inventory_id: { $regex: inventory_id, $options: 'i' },
-        category: { $regex: category, $options: 'i' },
-        condition: conditions ? { $in: conditions.split(',') } : { $regex: /.*/},
-        description: { $regex: description, $optionis: 'i' }
-    })
+    await Item.find(itemFilter)
         .limit(limit)
         .skip(page * limit)
         .collation({locale: 'de'})
         .sort({name: 'asc'})
         .then(items => {
             if (items.length === 0) {
-                res.json({ items: [], total: 0});
+                res.json({ items: [], total: 0, filter: itemFilter });
             }
             else {
-                Item.count({})
+                Item.count(itemFilter)
                     .then(
-                        count => res.json({ items: items, total: count })
+                        count => res.json({ items: items, total: count, filter: itemFilter })
                     ).catch(
                         err => res.status(400).json('Error: ' + err)
                     );
